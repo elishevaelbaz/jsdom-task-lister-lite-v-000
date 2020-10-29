4a9bfe64b39453c929103195b4b36de33e2541bb
github username <31523060+elishevaelbaz@users.noreply.github.com>
2020-10-28 21:04:35 -0400

    add event listener for submitting a new task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const form = document.querySelector("form");
+  const tasks = document.querySelector("#tasks") //document.getElementById("tasks")
+  const newTask = document.querySelector("#new-task-description")
+
+  form.addEventListener("submit", event => {
+
+    event.preventDefault()
+    console.log("hello")
+
+    tasks.innerHTML += `<li>${newTask.value}</li>`
+    newTask.value = ""
+  })
+
+
 });
